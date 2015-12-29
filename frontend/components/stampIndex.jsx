d1fd79338ba7b5cd6edd84be4373d5d840c5ccf2
Jonathan Reis <jon.michel.reis@gmail.com>
2015-12-29 15:41:21 -0800

    still angry

--- frontend/components/stampIndex.jsx
+++ frontend/components/stampIndex.jsx
@@ -20,12 +20,40 @@ var StampIndex = React.createClass({
     var allStamps = StampStore.all().reverse();
     this.setState({stamps: allStamps});
   },
+  // mergeSort: function(array, comparator) {
+  //   if (array.length < 2) {
+  //     return array;
+  //   }
+  //
+  //   var middle = Math.floor(array.length / 2);
+  //   var left = array.slice(0, middle);
+  //   var right = array.slice(middle);
+  //
+  //   return this.merge(
+  //     this.mergeSort(left, comparator), this.mergeSort(right, comparator), comparator
+  //   );
+  // },
+  // merge: function(left, right, comparator) {
+  //   var result = [];
+  //   var i = 0;
+  //   var j = 0;
+  //
+  //   while (i < left.length && j < right.length) {
+  //     if (comparator(left[i], right[j]) === -1)
+  //   }
+  // },
 
   render: function() {
     var stampsList = "";
     if (this.state.stamps) {
       var sortedStamps = this.state.stamps.sort(function(a, b) {
-        return a.stamp_uses < b.stamp_uses;
+        if (a.stamp_uses.length < b.stamp_uses.length) {
+          return 1;
+        } else if (a.stamp_uses.length === b.stamp_uses.length) {
+          return 0;
+        } else {
+          return -1;
+        }
       });
       stampsList = sortedStamps.map(function(stamp, idx){
         return (
